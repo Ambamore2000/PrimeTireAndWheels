efee2bcb2040937b340fce00970213ffe9b3e449
ambamore2000 <a_kim86@u.pacific.edu>
2022-11-11 21:07:29 -0800

    check if form is post

--- reviews_process.php
+++ reviews_process.php
@@ -2,3 +2,7 @@
 
 $rating = $first_name = $last_name = $email = $review_message = "";
 $rating_error = $first_name_error = $last_name_error = $email_error = $review_message_error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+}
