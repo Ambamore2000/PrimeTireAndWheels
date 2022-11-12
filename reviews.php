6f4faf2341c80c0ca20e27c965b58413127e8004
ambamore2000 <a_kim86@u.pacific.edu>
2022-11-11 21:36:26 -0800

    delete reviews_process.php and move content to reviews.php

--- reviews.php
+++ reviews.php
@@ -1,4 +1,67 @@
-<?php include("reviews_process.php"); ?>
+<?php
+
+$rating = $first_name = $last_name = $email = $review_message = "";
+$rating_error = $first_name_error = $last_name_error = $email_error = $review_message_error = "";
+
+$is_valid_data = true;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["rating"])) {
+        $rating_error = "Rating is required.";
+        $is_valid_data = false;
+    } else {
+        $rating = $_POST["rating"];
+    }
+
+    if (empty($_POST["first_name"])) {
+        $first_name_error =  "First name is required.";
+        $is_valid_data = false;
+    } else {
+        $first_name = $_POST["first_name"];
+
+        if (!preg_match("/^[a-zA-Z]+$/", $first_name)) {
+            $first_name_error = "First name can only include alphabets.";
+            $is_valid_data = false;
+        }
+    }
+
+    if (!empty($_POST["last_name"])) {
+        $last_name = $_POST["last_name"];
+        if (!preg_match("/^[a-zA-Z]+$/", $last_name)) {
+            $last_name_error = "Last name can only include alphabets.";
+            $is_valid_data = false;
+        }
+    }
+
+    if (!empty($_POST["email"])) {
+        $email = $_POST["email"];
+        if (!preg_match("/^[\w]+@([\w]+\.)+[\w]{2,63}$/", $email)) {
+            $email_error =  "Email is invalid.";
+            $is_valid_data = false;
+        }
+    }
+
+    if (empty($_POST["review_message"])) {
+        $review_message_error = "Review message is required.";
+        $is_valid_data = false;
+    } else {
+        $review_message = $_POST["review_message"];
+    }
+
+    if ($is_valid_data) {
+        $full_name = $first_name;
+        if ($last_name) {
+            $full_name .= " " . $last_name;
+        }
+        if ($email) {
+            $full_name .= " (" . $email . ")";
+        }
+
+        $processed_message = "Thank you " . $full_name . " for submitting your review. " .
+            "You have submitted a " . $rating . "/5 review with the message:" . $review_message;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -22,7 +85,7 @@
             <h2>RATING</h2>
             <!--TODO: Get Reviews Data; Sum(All Ratings)/5 = Rating -->
             <h2>LEAVE REVIEW</h2>
-            <form action="reviews_process.php" method="post">
+            <form action="" method="post">
                 <label><input type="text" name="first_name" placeholder="First Name"></label>
                 <label><input type="text" name="last_name" placeholder="Last Name"></label>
                 <br>
