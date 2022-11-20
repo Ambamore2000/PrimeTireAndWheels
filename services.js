5928caa1d7232ada7b0070ed16356b3edb1b4852
ambamore2000 <andkim123@gmail.com>
2022-11-20 14:24:20 -0800

    add transition for images

--- services.js
+++ services.js
@@ -1,7 +1,54 @@
+let badVariable = 8;
+
 function doThis() {
-    alert("OKKKKAAAAYYY");
+    let carsDiv = document.getElementById("cars_img");
+    let carsList = carsDiv.children;
+
+    if (badVariable === 0) {
+        let i;
+
+        for (i = 32 - 8; i < 32; i++) {
+            let car = carsList[i];
+            car.className = "cars_hidden";
+        }
+
+        for (i = 0; i < 8; i++) {
+            let car = carsList[i];
+            car.className = "cars_show";
+        }
+    } else {
+        let i;
+
+        for (i = badVariable - 8; i < badVariable; i++) {
+            let car = carsList[i];
+            car.className = "cars_hidden";
+        }
+
+        for (i = badVariable; i < badVariable + 8; i++) {
+            let car = carsList[i];
+            car.className = "cars_show";
+        }
+    }
+
+    badVariable += 8;
+
+    if (badVariable === 32) {
+        badVariable = 0;
+    }
 }
+
+function loadImages() {
+    let carsDiv = document.getElementById("cars_img");
+    let carsList = carsDiv.children;
+    let i;
+    for (i = 8; i < carsList.length; i++) {
+        let car = carsList[i];
+        car.className = "cars_hidden";
+    }
+}
+
 function attachHandlers() {
+    loadImages();
     document.getElementById("ok").onclick = doThis;
 }
 
