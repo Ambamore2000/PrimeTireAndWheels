de9d265c26bd130a8933febf013d557ab00938ac
ambamore2000 <andkim123@gmail.com>
2022-11-25 20:05:15 -0800

    fix services move function and styling

--- services.js
+++ services.js
@@ -1,78 +1,55 @@
 const COLUMN_COUNT = 8;
 
-function moveRight() {
-    let carsDiv = document.getElementById("cars_array");
-    let carsList = carsDiv.children;
+function move(input) {
+    let carsDiv = document.getElementById("cars_section");
+    let carsList = Array.from(carsDiv.children);
+    carsList = carsList.splice(1, carsList.length - 2);
 
     let firstInstanceOfShow = -1;
     let i;
-
     for (i = 0; i < carsList.length; i++) {
-        if (carsList[i].className === "cars_show") {
-            firstInstanceOfShow = i;
+        let elementToCheck = document.getElementById((i + 1).toString());
+        if (elementToCheck.className === "cars_show") {
+            firstInstanceOfShow = i + 1;
             break;
         }
     }
 
     for (i = firstInstanceOfShow; i < firstInstanceOfShow + COLUMN_COUNT; i++) {
-        carsList[i].className = "cars_hidden";
+        let elementToCheck = document.getElementById((i).toString());
+        elementToCheck.className = "cars_hidden";
     }
 
     let firstExpression;
     let secondExpression;
 
-    if (firstInstanceOfShow + COLUMN_COUNT >= carsList.length) {
-        firstExpression = 0;
-        secondExpression = COLUMN_COUNT;
-    } else {
-        firstExpression = firstInstanceOfShow + COLUMN_COUNT;
-        secondExpression = firstInstanceOfShow + COLUMN_COUNT*2;
-    }
-
-    for (i = firstExpression; i < secondExpression; i++) {
-        carsList[i].className = "cars_show";
-    }
-
-}
-
-function moveLeft() {
-    let carsDiv = document.getElementById("cars_array");
-    let carsList = carsDiv.children;
-
-    let firstInstanceOfShow = -1;
-    let i;
-
-    for (i = 0; i < carsList.length; i++) {
-        if (carsList[i].className === "cars_show") {
-            firstInstanceOfShow = i;
-            break;
+    if (input === "right") {
+        if (firstInstanceOfShow + COLUMN_COUNT >= carsList.length) {
+            firstExpression = 1;
+            secondExpression = COLUMN_COUNT + 1;
+        } else {
+            firstExpression = firstInstanceOfShow + COLUMN_COUNT;
+            secondExpression = firstInstanceOfShow + COLUMN_COUNT * 2;
+        }
+    } else if (input === "left") {
+        if (firstInstanceOfShow === 1) {
+            firstExpression = carsList.length + 1 - COLUMN_COUNT;
+            secondExpression = carsList.length + 1;
+        } else {
+            firstExpression = firstInstanceOfShow - COLUMN_COUNT;
+            secondExpression = firstInstanceOfShow;
         }
     }
 
-    for (i = firstInstanceOfShow; i < firstInstanceOfShow + COLUMN_COUNT; i++) {
-        carsList[i].className = "cars_hidden";
-    }
-
-    let firstExpression;
-    let secondExpression;
-
-    if (firstInstanceOfShow === 0) {
-        firstExpression = carsList.length - COLUMN_COUNT;
-        secondExpression = carsList.length;
-    } else {
-        firstExpression = firstInstanceOfShow - COLUMN_COUNT;
-        secondExpression = firstInstanceOfShow;
-    }
-
     for (i = firstExpression; i < secondExpression; i++) {
-        carsList[i].className = "cars_show";
+        let elementToCheck = document.getElementById((i).toString());
+        elementToCheck.className = "cars_show";
     }
-
 }
 
 function attachHandlers() {
-    document.getElementById("right_button").onclick = moveRight;
-    document.getElementById("left_button").onclick = moveLeft;
+    document.getElementById("right_button").onclick = function () { move("right") };
+    document.getElementById("left_button").onclick = function () { move("left") };
 }
 
 window.addEventListener("load", attachHandlers, true);
--- services.php
+++ services.php
@@ -33,42 +33,108 @@
         <div class="section">
             <h2>CARS WE SERVICE</h2>
             <div id="cars_section">
-                <input class="cars_button" id="left_button" type="image" src="/img/left.png"  alt="Left"/>
-                <div id="cars_array">
+                <div>
+                    <input class="cars_button" id="left_button" type="image" src="/img/left.png"  alt="Left"/>
+                </div>
+                <div>
                     <img id="1" src="img/cars/red_square.png" alt="Car" class="cars_show">
+                </div>
+                <div>
                     <img id="2" src="img/cars/blue_square.png" alt="Car" class="cars_show">
+                </div>
+                <div>
                     <img id="3" src="img/cars/green_square.png" alt="Car" class="cars_show">
+                </div>
+                <div>
                     <img id="4" src="img/cars/red_square.png" alt="Car" class="cars_show">
+                </div>
+                <div>
                     <img id="5" src="img/cars/blue_square.png" alt="Car" class="cars_show">
+                </div>
+                <div>
                     <img id="6" src="img/cars/green_square.png" alt="Car" class="cars_show">
+                </div>
+                <div>
                     <img id="7" src="img/cars/red_square.png" alt="Car" class="cars_show">
+                </div>
+                <div>
                     <img id="8" src="img/cars/blue_square.png" alt="Car" class="cars_show">
+                </div>
+                <div>
                     <img id="9" src="img/cars/green_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="10" src="img/cars/red_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="11" src="img/cars/blue_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="12" src="img/cars/green_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="13" src="img/cars/red_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="14" src="img/cars/blue_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="15" src="img/cars/green_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="16" src="img/cars/red_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="17" src="img/cars/blue_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="18" src="img/cars/green_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="19" src="img/cars/red_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="20" src="img/cars/blue_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="21" src="img/cars/green_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="22" src="img/cars/red_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="23" src="img/cars/blue_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="24" src="img/cars/green_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="25" src="img/cars/red_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="26" src="img/cars/blue_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="27" src="img/cars/green_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="28" src="img/cars/red_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="29" src="img/cars/blue_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="30" src="img/cars/green_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="31" src="img/cars/red_square.png" alt="Car" class="cars_hidden">
+                </div>
+                <div>
                     <img id="32" src="img/cars/blue_square.png" alt="Car" class="cars_hidden">
                 </div>
-                <input class="cars_button" id="right_button" type="image" src="/img/right.png"  alt="Right"/>
+                <div>
+                    <input class="cars_button" id="right_button" type="image" src="/img/right.png"  alt="Right"/>
+                </div>
             </div>
         </div>
 
